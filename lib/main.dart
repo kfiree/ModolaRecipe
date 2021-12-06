a502a0447e9568565be3bc102475dbcb07b8f8da
kfire <kfirettinger@gmail.com>
2021-12-06 17:26:24 +0200

    add recipe card and search boxes in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@ import 'views/home.dart';
 import 'views/recipe_view.dart';
 
 void main() {
-  runApp(ModolaRecipe());
+  runApp(ModulaRecipe());
 }
 
-class ModolaRecipe extends StatelessWidget {
-  // const ModolaRecipe({Key? key}) : super(key: key);
+class ModulaRecipe extends StatelessWidget {
+  // const ModulaRecipe({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ModolaRecipe',
+      title: 'ModulaRecipe',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,13 +1,24 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  // const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController textEditingController = new TextEditingController();
+
+  String applicationId = "41ca25af";
+  String applicationKey = "ab51bad1b862188631ce612a9b1787a9";
+
+  // search for recipe
+  search(String query) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,25 +27,29 @@ class _HomeState extends State<Home> {
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Colors.deepPurpleAccent, Colors.blue])),
+              gradient: LinearGradient(colors: [Colors.blue, Colors.black87])),
         ),
         Container(
-            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-            child: Column(children: <Widget>[
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
+                children: <Widget>[
                   Text(
-                    "ModolaR",
-                    style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
+                    "Modula",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
                     textAlign: TextAlign.center,
                   ),
+                  GradientText("R",
+                      style: TextStyle(fontSize: 35),
+                      colors: const [Colors.black, Colors.white]),
                   Text(
-                    "Recipies",
+                    "ecipies",
                     style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 23,
+                        color: Colors.white,
+                        fontSize: 30,
                         fontWeight: FontWeight.w500),
                   )
                 ],
@@ -42,9 +57,86 @@ class _HomeState extends State<Home> {
               SizedBox(
                 height: 30,
               ),
-              Text("What will you cock today?"),
-              Text("So.. what ingredients do you have?")
-            ])),
+              Text(
+                "What will you cock today?",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "Insert what ingredients you have?",
+                style: TextStyle(fontSize: 13, color: Colors.white),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: textEditingController,
+                        decoration: InputDecoration(
+                            hintText: "Enter Ingrideints",
+                            hintStyle: TextStyle(
+                              fontSize: 18,
+                            )),
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        if (textEditingController.text.isNotEmpty) {
+                          print("just do it");
+                        } else {
+                          print(" dont");
+                        }
+                      },
+                      child: Container(
+                        child: Icon(Icons.search, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: textEditingController,
+                        decoration: InputDecoration(
+                            hintText: "Enter Recipe Name",
+                            hintStyle: TextStyle(
+                              fontSize: 18,
+                            )),
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        if (textEditingController.text.isNotEmpty) {
+                          print("just do it");
+                        } else {
+                          print(" dont");
+                        }
+                      },
+                      child: Container(
+                        child: Icon(Icons.search, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ]),
     );
   }
