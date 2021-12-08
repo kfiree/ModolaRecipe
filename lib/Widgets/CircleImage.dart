6b203e3ad3b6121f980b8d38b47e8a9f84cc9d76
hajajon <amitha@wix.com>
2021-12-08 17:26:48 +0200

    added a recipe page. Still needs to be designeed as part of out project

---
+++ lib/Widgets/CircleImage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CircleNetworkImage extends StatelessWidget {
+  final double radius;
+  final String imageURL;
+
+  CircleNetworkImage({required this.radius, required this.imageURL});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: radius,
+      height: radius,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: NetworkImage(
+            imageURL,
+          ),
+        ),
+      ),
+    );
+  }
+}
